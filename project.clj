3e5e6f510fde28f528971b8da9ed09892fe8db39
Jon Eisen <jon@joneisen.me>
2016-07-09 23:08:42 -0600

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.yanatan16/marathonclj "0.1.2-SNAPSHOT"
+(defproject org.clojars.yanatan16/marathonclj "0.1.2"
   :description "Marathon Rest Client for Clojure "
   :url "https://github.com/codemomentum/marathonclj"
   :dependencies [[org.clojure/clojure "1.7.0"]
